e4313cfcc2a7e029e2fd32a3770a39df59942913
sunilt4 <sunil.tumkur@uoit.net>
2020-03-02 21:06:03 -0500

    Update TicTacToe.java

--- Game/TicTacToe.java
+++ Game/TicTacToe.java
@@ -11,6 +11,7 @@ public class TicTacToe {
     public  char turn = 'X';
 
 
+    //MAIN METHOD
     public static void main(String[] args) {
         // write your code here
         TicTacToe t1 = new TicTacToe();
@@ -26,32 +27,36 @@ public class TicTacToe {
 
     }
 
+    //PLAYING THE GAME, INVOLVES USER INPUT
     public void Play() {
 
         boolean playing = true;
 
         PrintBoard();
+        System.out.println("Please enter a row and a column: ");
 
         while (playing) {
 
-            System.out.println("Please enter a row and a column: ");
+            //System.out.println("Please enter a row and a column: ");
 
-            boolean entryValid = false;
+           row = scan.nextInt() - 1;
+           col = scan.nextInt() - 1;
 
-            while (!entryValid) {
-                row = scan.nextInt() - 1;
-                col = scan.nextInt() - 1;
 
-                try {
-                    board[row][col] = turn;
-                }
-                catch (java.lang.ArrayIndexOutOfBoundsException e) {
-                    System.out.println("Out of bounds. Reenter.");
-                    continue;
-                }
 
-                entryValid = true;
+           if ((row < 0 || row > 2) && (col < 0 || col > 2))  {
+                throw new java.lang.ArrayIndexOutOfBoundsException("Row and Column value is outside board");
+                //System.out.println("Place is outside the board");
             }
+
+            if ((board[row][col] != '_' )) {
+                throw new java.lang.RuntimeException("Place is occupied");
+               // System.out.println("Place is occupied");
+
+            }
+
+             board[row][col] = turn;
+
 
             if (GameOver(row, col)) {
                 playing = false;
@@ -69,26 +74,30 @@ public class TicTacToe {
         }
     }
 
+    //PRINTING THE BOARD
     public void PrintBoard() {
         for (int i=0; i<3; i++) {
+            System.out.println();
             for (int j=0; j<3; j++) {
                 if (j==0) {
                     System.out.print("| ");
                 }
                 System.out.print(board[i][j] + " | ");
             }
-            System.out.println();
         }
         System.out.println();
     }
 
+    //CHECKING TO SEE WHO WINS THE GAME
     public boolean GameOver(int rMove, int cMove) {
 
+        //CHECKING FOR ALL POSSIBILITIES OF VICTORY
+
         //check for perpendicular victory
-        if (board[0][cMove] == board[1][cMove] && board[0][cMove] == board[2][cMove] && board[0][cMove] != '_') {
+        if (board[0][cMove] == board[1][cMove] && board[0][cMove] == board[2][cMove]) {
             return true;
         }
-        if (board[rMove][0] == board[rMove][1] && board[rMove][0] == board[rMove][2] && board[rMove][0] != '_') {
+        if (board[rMove][0] == board[rMove][1] && board[rMove][0] == board[rMove][2]) {
             return true;
         }
         //check for diagonal victory
@@ -103,3 +112,4 @@ public class TicTacToe {
 
     }
 }
+
